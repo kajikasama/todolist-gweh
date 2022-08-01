980f1d46138d71cf0cfd0ad5e794f7a59c90c84a
kajikasama <aguschandravii4@gmail.com>
2022-08-02 03:26:51 +0800

    first commit

---
+++ src/TodoList.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+let id = 0
+const newTodo = ref('')
+const todos = ref([
+    {id: id++, text: 'Ngocok'},
+    {id: id++, text: 'Ngoding'},
+    {id: id++, text: 'Ngehalu'}
+])
+
+function addTodo(){
+    todos.value.push({ id: id++, text: newTodo.value })
+}
+
+function removeTodo(todo){
+    todos.value = todos.value.filter((hapus)=> hapus !== todo );
+}
+
+</script>
+
+<template>
+<h1>kegiatan hari ini todo list aneh-aneh</h1>
+    <form @submit.prevent="addTodo">
+        <input type="text" name="" id="" v-model="newTodo">
+        <button>Add Todo</button>
+    </form>
+    <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            {{ todo.text }}
+            <button @click="removeTodo(todo)">x</button>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+import TodoList from './TodoList.vue'
+
+// import './assets/main.css'
+import './assets/gw.css'
+
+// createApp(App).mount('#app')
+createApp(TodoList).mount('#todolist');
